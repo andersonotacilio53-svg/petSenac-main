252cc7498c1e6f9ebf932614a11b49e6b8a74f82
Otacilio <andersonotacilio53@gmail.com>
2025-09-23 22:10:32 -0300

    atualizar

--- petSenac/FormUsuario.Designer.cs
+++ petSenac/FormUsuario.Designer.cs
@@ -28,12 +28,293 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgvLista = new System.Windows.Forms.DataGridView();
+            this.grbCadastro = new System.Windows.Forms.GroupBox();
+            this.txbSenha = new System.Windows.Forms.TextBox();
+            this.txbEmail = new System.Windows.Forms.TextBox();
+            this.txbNome = new System.Windows.Forms.TextBox();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblNomeCompleto = new System.Windows.Forms.Label();
+            this.grbEditar = new System.Windows.Forms.GroupBox();
+            this.txbSenhaEditar = new System.Windows.Forms.TextBox();
+            this.txbEmailEditar = new System.Windows.Forms.TextBox();
+            this.txbNomeEditar = new System.Windows.Forms.TextBox();
+            this.lblSenhaEditar = new System.Windows.Forms.Label();
+            this.lblEmailEditar = new System.Windows.Forms.Label();
+            this.lblNomeEditar = new System.Windows.Forms.Label();
+            this.grbApagar = new System.Windows.Forms.GroupBox();
+            this.btnApagar = new System.Windows.Forms.Button();
+            this.txbDescricao = new System.Windows.Forms.TextBox();
+            this.lblDescricaoLogo = new System.Windows.Forms.Label();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLista)).BeginInit();
+            this.grbCadastro.SuspendLayout();
+            this.grbEditar.SuspendLayout();
+            this.grbApagar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvLista
+            // 
+            this.dgvLista.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dgvLista.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLista.Location = new System.Drawing.Point(44, 117);
+            this.dgvLista.Name = "dgvLista";
+            this.dgvLista.Size = new System.Drawing.Size(856, 244);
+            this.dgvLista.TabIndex = 0;
+          //  this.dgvLista.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvlista_CellClick);
+            // 
+            // grbCadastro
+            // 
+            this.grbCadastro.Controls.Add(this.btnCadastrar);
+            this.grbCadastro.Controls.Add(this.txbSenha);
+            this.grbCadastro.Controls.Add(this.txbEmail);
+            this.grbCadastro.Controls.Add(this.txbNome);
+            this.grbCadastro.Controls.Add(this.lblSenha);
+            this.grbCadastro.Controls.Add(this.lblEmail);
+            this.grbCadastro.Controls.Add(this.lblNomeCompleto);
+            this.grbCadastro.Location = new System.Drawing.Point(44, 382);
+            this.grbCadastro.Name = "grbCadastro";
+            this.grbCadastro.Size = new System.Drawing.Size(351, 223);
+            this.grbCadastro.TabIndex = 1;
+            this.grbCadastro.TabStop = false;
+            this.grbCadastro.Text = "Cadastro";
+            // 
+            // txbSenha
+            // 
+            this.txbSenha.Location = new System.Drawing.Point(96, 111);
+            this.txbSenha.Name = "txbSenha";
+            this.txbSenha.Size = new System.Drawing.Size(224, 20);
+            this.txbSenha.TabIndex = 5;
+            // 
+            // txbEmail
+            // 
+            this.txbEmail.Location = new System.Drawing.Point(90, 74);
+            this.txbEmail.Name = "txbEmail";
+            this.txbEmail.Size = new System.Drawing.Size(230, 20);
+            this.txbEmail.TabIndex = 4;
+            // 
+            // txbNome
+            // 
+            this.txbNome.Location = new System.Drawing.Point(89, 34);
+            this.txbNome.Name = "txbNome";
+            this.txbNome.Size = new System.Drawing.Size(230, 20);
+            this.txbNome.TabIndex = 3;
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSenha.Location = new System.Drawing.Point(19, 108);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(70, 24);
+            this.lblSenha.TabIndex = 2;
+            this.lblSenha.Text = "Senha:";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Location = new System.Drawing.Point(19, 71);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(62, 24);
+            this.lblEmail.TabIndex = 1;
+            this.lblEmail.Text = "Email:";
+            // 
+            // lblNomeCompleto
+            // 
+            this.lblNomeCompleto.AutoSize = true;
+            this.lblNomeCompleto.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNomeCompleto.Location = new System.Drawing.Point(16, 30);
+            this.lblNomeCompleto.Name = "lblNomeCompleto";
+            this.lblNomeCompleto.Size = new System.Drawing.Size(67, 24);
+            this.lblNomeCompleto.TabIndex = 0;
+            this.lblNomeCompleto.Text = "Nome:";
+            // 
+            // grbEditar
+            // 
+            this.grbEditar.Controls.Add(this.btnEditar);
+            this.grbEditar.Controls.Add(this.txbSenhaEditar);
+            this.grbEditar.Controls.Add(this.txbEmailEditar);
+            this.grbEditar.Controls.Add(this.txbNomeEditar);
+            this.grbEditar.Controls.Add(this.lblSenhaEditar);
+            this.grbEditar.Controls.Add(this.lblEmailEditar);
+            this.grbEditar.Controls.Add(this.lblNomeEditar);
+            this.grbEditar.Location = new System.Drawing.Point(549, 382);
+            this.grbEditar.Name = "grbEditar";
+            this.grbEditar.Size = new System.Drawing.Size(351, 223);
+            this.grbEditar.TabIndex = 2;
+            this.grbEditar.TabStop = false;
+            this.grbEditar.Text = "Editar";
+            // 
+            // txbSenhaEditar
+            // 
+            this.txbSenhaEditar.Location = new System.Drawing.Point(96, 111);
+            this.txbSenhaEditar.Name = "txbSenhaEditar";
+            this.txbSenhaEditar.Size = new System.Drawing.Size(224, 20);
+            this.txbSenhaEditar.TabIndex = 5;
+            // 
+            // txbEmailEditar
+            // 
+            this.txbEmailEditar.Location = new System.Drawing.Point(90, 74);
+            this.txbEmailEditar.Name = "txbEmailEditar";
+            this.txbEmailEditar.Size = new System.Drawing.Size(230, 20);
+            this.txbEmailEditar.TabIndex = 4;
+            // 
+            // txbNomeEditar
+            // 
+            this.txbNomeEditar.Location = new System.Drawing.Point(90, 33);
+            this.txbNomeEditar.Name = "txbNomeEditar";
+            this.txbNomeEditar.Size = new System.Drawing.Size(230, 20);
+            this.txbNomeEditar.TabIndex = 3;
+            // 
+            // lblSenhaEditar
+            // 
+            this.lblSenhaEditar.AutoSize = true;
+            this.lblSenhaEditar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSenhaEditar.Location = new System.Drawing.Point(19, 108);
+            this.lblSenhaEditar.Name = "lblSenhaEditar";
+            this.lblSenhaEditar.Size = new System.Drawing.Size(70, 24);
+            this.lblSenhaEditar.TabIndex = 2;
+            this.lblSenhaEditar.Text = "Senha:";
+            // 
+            // lblEmailEditar
+            // 
+            this.lblEmailEditar.AutoSize = true;
+            this.lblEmailEditar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmailEditar.Location = new System.Drawing.Point(19, 71);
+            this.lblEmailEditar.Name = "lblEmailEditar";
+            this.lblEmailEditar.Size = new System.Drawing.Size(62, 24);
+            this.lblEmailEditar.TabIndex = 1;
+            this.lblEmailEditar.Text = "Email:";
+            // 
+            // lblNomeEditar
+            // 
+            this.lblNomeEditar.AutoSize = true;
+            this.lblNomeEditar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNomeEditar.Location = new System.Drawing.Point(16, 30);
+            this.lblNomeEditar.Name = "lblNomeEditar";
+            this.lblNomeEditar.Size = new System.Drawing.Size(67, 24);
+            this.lblNomeEditar.TabIndex = 0;
+            this.lblNomeEditar.Text = "Nome:";
+            // 
+            // grbApagar
+            // 
+            this.grbApagar.Controls.Add(this.btnApagar);
+            this.grbApagar.Controls.Add(this.txbDescricao);
+            this.grbApagar.Location = new System.Drawing.Point(64, 636);
+            this.grbApagar.Name = "grbApagar";
+            this.grbApagar.Size = new System.Drawing.Size(856, 100);
+            this.grbApagar.TabIndex = 3;
+            this.grbApagar.TabStop = false;
+            this.grbApagar.Text = "Apagar";
+            // 
+            // btnApagar
+            // 
+            this.btnApagar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnApagar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnApagar.ForeColor = System.Drawing.Color.Red;
+            this.btnApagar.Location = new System.Drawing.Point(699, 38);
+            this.btnApagar.Name = "btnApagar";
+            this.btnApagar.Size = new System.Drawing.Size(138, 53);
+            this.btnApagar.TabIndex = 1;
+            this.btnApagar.Text = "Apagar";
+            this.btnApagar.UseVisualStyleBackColor = true;
+          //  this.btnApagar.Click += new System.EventHandler(this.btnApagar_Click);
+            // 
+            // txbDescricao
+            // 
+            this.txbDescricao.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbDescricao.Location = new System.Drawing.Point(54, 49);
+            this.txbDescricao.Name = "txbDescricao";
+            this.txbDescricao.Size = new System.Drawing.Size(354, 29);
+            this.txbDescricao.TabIndex = 0;
+            this.txbDescricao.Text = "Selecione o Usuário que deseja Apagar!";
+            // 
+            // lblDescricaoLogo
+            // 
+            this.lblDescricaoLogo.AutoSize = true;
+            this.lblDescricaoLogo.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescricaoLogo.Location = new System.Drawing.Point(219, 26);
+            this.lblDescricaoLogo.Name = "lblDescricaoLogo";
+            this.lblDescricaoLogo.Size = new System.Drawing.Size(483, 42);
+            this.lblDescricaoLogo.TabIndex = 4;
+            this.lblDescricaoLogo.Text = "Gerenciamento de Usuários";
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCadastrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastrar.ForeColor = System.Drawing.Color.Red;
+            this.btnCadastrar.Location = new System.Drawing.Point(96, 149);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(224, 57);
+            this.btnCadastrar.TabIndex = 6;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+          //  this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEditar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditar.ForeColor = System.Drawing.Color.Red;
+            this.btnEditar.Location = new System.Drawing.Point(90, 160);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(224, 57);
+            this.btnEditar.TabIndex = 7;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+          //  this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // FormUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormUsuario";
+            this.BackgroundImage = global::petSenac.Properties.Resources.pet;
+            this.ClientSize = new System.Drawing.Size(1019, 818);
+            this.Controls.Add(this.lblDescricaoLogo);
+            this.Controls.Add(this.grbApagar);
+            this.Controls.Add(this.grbEditar);
+            this.Controls.Add(this.grbCadastro);
+            this.Controls.Add(this.dgvLista);
+            this.Name = "FormUsuario";
+            this.Text = "Usuario";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLista)).EndInit();
+            this.grbCadastro.ResumeLayout(false);
+            this.grbCadastro.PerformLayout();
+            this.grbEditar.ResumeLayout(false);
+            this.grbEditar.PerformLayout();
+            this.grbApagar.ResumeLayout(false);
+            this.grbApagar.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvLista;
+        private System.Windows.Forms.GroupBox grbCadastro;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblNomeCompleto;
+        private System.Windows.Forms.TextBox txbSenha;
+        private System.Windows.Forms.TextBox txbEmail;
+        private System.Windows.Forms.TextBox txbNome;
+        private System.Windows.Forms.GroupBox grbEditar;
+        private System.Windows.Forms.TextBox txbSenhaEditar;
+        private System.Windows.Forms.TextBox txbEmailEditar;
+        private System.Windows.Forms.TextBox txbNomeEditar;
+        private System.Windows.Forms.Label lblSenhaEditar;
+        private System.Windows.Forms.Label lblEmailEditar;
+        private System.Windows.Forms.Label lblNomeEditar;
+        private System.Windows.Forms.GroupBox grbApagar;
+        private System.Windows.Forms.TextBox txbDescricao;
+        private System.Windows.Forms.Button btnApagar;
+        private System.Windows.Forms.Label lblDescricaoLogo;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.Button btnEditar;
     }
 }
---
+++ petSenac/Model/OrdemServico.cs
@@ -0,0 +1,126 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace petSenac.Model
+{
+    internal class OrdemServico
+    {
+        public int Id { get; set; }
+        public int Id_animal { get; set; }
+        public int Id_funcionarios { get; set; }
+        public string IdFicha { get; set; }
+        public DateTime data { get; set; }
+
+        public DataTable listar()
+        {
+            string comando = "SELECT * FROM ordens_servico";
+
+            Banco conexaoBD = new Banco();
+            MySqlConnection con = conexaoBD.ObterConexao();
+            MySqlCommand cmd = new MySqlCommand(comando, con);
+            cmd.Prepare();
+
+
+            DataTable tabela = new DataTable();
+
+            tabela.Load(cmd.ExecuteReader());
+            conexaoBD.Desconectar(con);
+            return tabela;
+
+
+
+        }
+        public DataTable BuscarPorFicha()
+        {
+            string comando = "SELECT * FROM ordens_servico HERE Ficha = @idFicha";
+
+            Banco conexaoBD = new Banco();
+            MySqlConnection con = conexaoBD.ObterConexao();
+            MySqlCommand cmd = new MySqlCommand(comando, con);
+            cmd.Parameters.AddWithValue("@IdFicha", IdFicha);
+
+
+
+            cmd.Prepare();
+
+
+            DataTable tabela = new DataTable();
+
+            tabela.Load(cmd.ExecuteReader());
+            conexaoBD.Desconectar(con);
+            return tabela;
+        }
+        public bool cadastrar()
+        {
+            string comando = "INSERT INTO ordens_servico (id_ficha,id_animal,id_funcionarios,data) VALUES " + "(@id_ficha,@id_produto,@quantidade,@id_resp)";
+            Banco conexaoBD = new Banco();
+            MySqlConnection con = conexaoBD.ObterConexao();
+            MySqlCommand cmd = new MySqlCommand(comando, con);
+            cmd.Parameters.AddWithValue("@id_animal", Id_animal);
+            cmd.Parameters.AddWithValue("@id_funcionario", Id_funcionarios);
+            cmd.Parameters.AddWithValue("@Data", data);
+
+
+            cmd.Prepare();
+            // O trecho abaixo irá retornar true caso o cadastro dê certo:
+            // Em caso de erro, experimente comentar o try/catch e executar novamente o código;
+            // Grande parte dos problemas estão associados à um comando SQL incorreto. Verifique a string comando.
+            try
+            {
+                if (cmd.ExecuteNonQuery() == 0)
+                {
+                    conexaoBD.Desconectar(con);
+                    return false;
+                }
+                else
+                {
+                    conexaoBD.Desconectar(con);
+                    return true;
+                }
+            }
+            catch
+            {
+                conexaoBD.Desconectar(con);
+                return false;
+            }
+        }
+        public bool EncerrarComanda()
+        {
+            string comando = "UPDATE ordens_comandas SET situacao = 0 " + "WHERE id_ficha = @Id_ficha AND situacao = 1";
+            Banco conexaoBD = new Banco();
+            MySqlConnection con = conexaoBD.ObterConexao();
+            MySqlCommand cmd = new MySqlCommand(comando, con);
+            cmd.Parameters.AddWithValue("@id_ficha", IdFicha);
+
+
+            cmd.Prepare();
+            // O trecho abaixo irá retornar true caso o cadastro dê certo:
+            // Em caso de erro, experimente comentar o try/catch e executar novamente o código;
+            // Grande parte dos problemas estão associados à um comando SQL incorreto. Verifique a string comando.
+            try
+            {
+                if (cmd.ExecuteNonQuery() == 0)
+                {
+                    conexaoBD.Desconectar(con);
+                    return false;
+                }
+                else
+                {
+                    conexaoBD.Desconectar(con);
+                    return true;
+                }
+            }
+            catch
+            {
+                conexaoBD.Desconectar(con);
+                return false;
+            }
+
+        }
+    }
+}
